e98fabc77ec58a2f3058cec70dd9674705d9e0f6
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-11-21 11:05:27 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,61 @@
+// const User = require('../models/userModel');
+
+// const paginate = async (req, res) => {
+//   try {
+//     // TODO: Query the users based on the pagination parameters
+//     // TODO: Respond with the paginated users
+//   } catch (error) {
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
+
+// const sort = async (req, res) => {
+//   const sortOptions = {};
+
+//   if (sort) {
+//     // TODO: Define sorting options based on the 'sort' query parameter
+//   }
+
+//   try {
+//     // TODO: Query and sort the products based on the sorting options
+//     // TODO: Respond with the sorted products
+//   } catch (error) {
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// };
+
+// module.exports = {
+//   paginate,
+//   sort,
+// };
+
 const User = require('../models/userModel');
 
 const paginate = async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+
   try {
-    // TODO: Query the users based on the pagination parameters
-    // TODO: Respond with the paginated users
+    const users = await User.find()
+      .skip((page - 1) * limit)
+      .limit(parseInt(limit));
+    
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
 
 const sort = async (req, res) => {
+  const { sort } = req.query;
   const sortOptions = {};
 
   if (sort) {
-    // TODO: Define sorting options based on the 'sort' query parameter
+    sortOptions['name'] = sort === 'asc' ? 1 : -1;
   }
 
   try {
-    // TODO: Query and sort the products based on the sorting options
-    // TODO: Respond with the sorted products
+    const sortedUsers = await User.find().sort(sortOptions);
+    res.status(200).json(sortedUsers);
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
   }
@@ -28,3 +65,4 @@ module.exports = {
   paginate,
   sort,
 };
+
