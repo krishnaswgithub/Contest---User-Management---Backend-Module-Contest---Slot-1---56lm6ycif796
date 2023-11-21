1223921083454e489c1824f82398474f6a029d47
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-11-21 09:35:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+// const express = require('express');
+// const app = express();
+// const router = require('../routes/userRoutes');
+
+// app.use(express.json());
+
+// app.use('/api/v1', router);
+
+// module.exports = app;
+
 const express = require('express');
 const app = express();
 const router = require('../routes/userRoutes');
 
 app.use(express.json());
 
+// Use the user routes
 app.use('/api/v1', router);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+});
 
 module.exports = app;
