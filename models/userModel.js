92c5161d8adeeca6590064f7bbff2a5ea9bc7d3a
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-11-21 11:04:35 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,39 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//   // 'name' field is of type String
+//   // It is required (cannot be empty)
+//   // 'age' field is of type Number
+//   // It is required (cannot be empty)
+//   // 'course' field is of type String
+//   // It is required (cannot be empty)
+//   // 'experience' field is of type Boolean
+//   // It has a default value of 'true'
+// });
+
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = User;
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  // 'name' field is of type String
-  // It is required (cannot be empty)
-  // 'age' field is of type Number
-  // It is required (cannot be empty)
-  // 'course' field is of type String
-  // It is required (cannot be empty)
-  // 'experience' field is of type Boolean
-  // It has a default value of 'true'
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  course: {
+    type: String,
+    required: true,
+  },
+  experience: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
